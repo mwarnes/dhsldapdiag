1ec1933c189b8c71e3e9949162d727409a57337b
Martin Warnes <martin.warnes@marklogic.com>
2020-12-03 15:26:23 +0200

    Add buildspec.yml

--- cmd/checkConfig.go
+++ cmd/checkConfig.go
@@ -26,7 +26,7 @@ import (
 )
 
 type roleStruct struct {
-	rolename string
+	rolename   string
 	externalDN []string
 }
 
@@ -35,7 +35,9 @@ var accountuid string
 var accountdn string
 var accountpword string
 var verified bool
-var logmsg color.Style
+
+//var logmsg color.Style
+var logmsg color.RGBColor
 
 var checkConfig = &cobra.Command{
 	Use:   "checkconfig",
@@ -58,22 +60,23 @@ func init() {
 }
 
 func checkLdapConfiguration() {
-	logmsg = color.New(color.FgBlue, color.OpBold)
+	//logmsg = color.New(color.FgBlue, color.OpBold)
+	logmsg = color.HEX("#1976D2")
 	if viper.GetBool("debug") {
 		log.Println(spew.Sdump(viper.AllSettings()))
 	}
 
 	// Check that we have all the required parameters and prompt for any missing values
-	admindn := validateField("admin-dn",false)
-	address := validateField("address",false)
-	serveruri := validateField("server-uri",false)
-	base := validateField("base",false)
-	defaultuser := validateField("default-user",false)
-	bindmethod := validateField("bind-method",false)
-	ldapattribute := validateField("ldap-attribute",false)
-	memberofattribute := validateField("memberof-attribute",false)
-	memberattribute := validateField("member-attribute",false)
-	password := validateField("default-user password",true)
+	admindn := validateField("admin-dn", false)
+	address := validateField("address", false)
+	serveruri := validateField("server-uri", false)
+	base := validateField("base", false)
+	defaultuser := validateField("default-user", false)
+	bindmethod := validateField("bind-method", false)
+	ldapattribute := validateField("ldap-attribute", false)
+	memberofattribute := validateField("memberof-attribute", false)
+	memberattribute := validateField("member-attribute", false)
+	password := validateField("default-user password", true)
 
 	// Display chosen values and prompt to continue
 	screen.Clear()
@@ -93,7 +96,7 @@ func checkLdapConfiguration() {
 
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"LDAP Field", "Value"})
-	table.SetColMinWidth(1,80)
+	table.SetColMinWidth(1, 80)
 	table.SetAutoWrapText(false)
 	table.SetColumnColor(tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlueColor},
 		tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor})
@@ -111,16 +114,16 @@ func checkLdapConfiguration() {
 
 	table = tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"Role", "External DN"})
-	table.SetColMinWidth(1,80)
+	table.SetColMinWidth(1, 80)
 	table.SetAutoWrapText(false)
 	table.SetColumnColor(tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlueColor},
 		tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor})
-	for _,role := range configJson.Roles {
+	for _, role := range configJson.Roles {
 		var extdn string
 		for i := 1; i < len(role); i++ {
 			extdn = extdn + role[i] + "\n"
 		}
-		table.Append([]string{role[0],extdn})
+		table.Append([]string{role[0], extdn})
 	}
 	table.Render()
 
@@ -156,25 +159,27 @@ func checkLdapConfiguration() {
 	}
 	hostname := u.Hostname()
 
-	success, failures := checkConnectivity(address,hostname,port)
+	success, failures := checkConnectivity(address, hostname, port)
 	if success == 0 {
 		color.Error.Println("Unable to connect to any LDAP Servers.")
 		os.Exit(0)
 	}
-	if failures>0 {
-		if failures==0 {
-			promptContinue(color.Success.Sprintf("%d Failures reported during connectivity test, do you want to continue.",failures),"N")
+	if failures > 0 {
+		if failures == 0 {
+			promptContinue(color.Success.Sprintf("%d Failures reported during connectivity test, do you want to continue.", failures), "N")
 		} else {
-			promptContinue(color.Danger.Sprintf("%d Failures reported during connectivity test, do you want to continue.",failures),"N")
+			promptContinue(color.Danger.Sprintf("%d Failures reported during connectivity test, do you want to continue.", failures), "N")
 		}
+	} else {
+		promptContinue(color.Question.Sprintf("Continue to check LDAP Default User bind"), "Y")
 	}
-	
-	conn := checkLdapAdminBind(hostname,port,isLdapSecure,bindmethod,defaultuser,password)
-	defaultUserConn := conn
-	
-	adminaccounts := checkAdminDN(conn,admindn,base, memberofattribute, ldapattribute)
 
-	verified = verifyAdminAccount(conn, adminaccounts)
+	conn := checkLdapAdminBind(hostname, port, isLdapSecure, bindmethod, defaultuser, password)
+	defaultUserConn := conn
+
+	adminaccounts := checkAdminDN(conn, admindn, base, memberofattribute, ldapattribute)
+
+	verified = verifyAdminAccount(hostname, port, isLdapSecure, bindmethod, adminaccounts)
 
 	checkRolesDN(defaultUserConn, configJson, base, memberofattribute, ldapattribute)
 	generateRolesRequests("curl")
@@ -223,15 +228,39 @@ func generateRolesRequests(format string) {
 
 }
 
-func verifyAdminAccount(conn *ldap.Conn, adminaccounts [][]string) bool {
+func verifyAdminAccount(hostname string, port string, secure bool, bindmethod string, adminaccounts [][]string) bool {
 	//color.Success.Println("Verifying Security Admin account access (optional)")
 	banner("Verifying Security Admin account access (optional)")
+	net.JoinHostPort(hostname, port)
+	var conn *ldap.Conn
+	var err error
+	var config *tls.Config
+	if secure {
+		config = &tls.Config{}
+		config.InsecureSkipVerify = true
+		logmsg.Println("Establishing secure connection to LDAP server.")
+		conn, err = ldap.DialTLS("tcp", net.JoinHostPort(hostname, port), config)
+		if viper.GetBool("debug") {
+			log.Println(spew.Sdump(conn.TLSConnectionState()))
+			conn.Debug.Enable(true)
+		}
+	} else {
+		logmsg.Println("Establishing connection to LDAP server.")
+		conn, err = ldap.Dial("tcp", net.JoinHostPort(hostname, port))
+		if viper.GetBool("debug") {
+			conn.Debug.Enable(true)
+		}
+	}
+	if err != nil {
+		color.Error.Printf("Failed to connect to LDAP Server. %s", err)
+		os.Exit(0)
+	}
 
 	var accounts []string
 	for _, account := range adminaccounts {
-		accounts = append(accounts,account[0])
+		accounts = append(accounts, account[0])
 	}
-	accounts = append(accounts,">>Skip<<")
+	accounts = append(accounts, ">>Skip<<")
 
 	sprompt := promptui.Select{
 		Label: logmsg.Sprintf("Select Security Account to verify"),
@@ -245,7 +274,7 @@ func verifyAdminAccount(conn *ldap.Conn, adminaccounts [][]string) bool {
 	}
 
 	if account == ">>Skip<<" {
-		return true
+		return false
 	}
 
 	for _, a := range adminaccounts {
@@ -256,25 +285,57 @@ func verifyAdminAccount(conn *ldap.Conn, adminaccounts [][]string) bool {
 		}
 	}
 
-	prompt := promptui.Prompt{
-		Label: color.FgBlue.Sprintf("Enter password for %s",account),
-		Mask:  '*',
-	}
-	accountpword, err = prompt.Run()
-	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
-		return false
-	}
+	//TODO Convert to a loop to allow for errors
+	var retval bool
+	for {
+		prompt := promptui.Prompt{
+			Label: color.FgBlue.Sprintf("Enter password for %s or X to quit", account),
+			Mask:  '*',
+		}
+		accountpword, err = prompt.Run()
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			return false
+		}
+		if strings.EqualFold(accountpword, "X") {
+			return false
+		}
 
-	if err := conn.Bind(accountdn, accountpword); err != nil {
-		color.Error.Printf("Failed to auth. %s", err)
-		os.Exit(0)
-		return false
-	} else {
-		color.Success.Println("Authenticated successfully!")
-		promptContinue(color.Question.Sprintf("Continue to search for DHS User Roles"),"Y")
-		return true
+		switch strings.ToUpper(bindmethod) {
+		case "SIMPLE":
+			logmsg.Println("Performing simple bind")
+			err = conn.Bind(accountdn, accountpword)
+			if err != nil {
+				color.Error.Printf("Failed to bind with supplied credentials. %s", err)
+			} else {
+				color.Success.Println("LDAP Admin credentials successfully verified")
+				promptContinue(color.Question.Sprintf("Continue to search for DHS User Roles"), "Y")
+				return true
+			}
+		case "MD5":
+			logmsg.Println("Performing MD5 bind")
+			err = conn.MD5Bind(hostname, accountdn, accountpword)
+			if err != nil {
+				if secure {
+					color.Error.Printf("MD5 Bind failed this could be because LDAPS is configured. %s", err)
+					color.Error.Printf("If you continue and the your credentials are incorrect further tests could fail.")
+					promptContinue(color.Question.Sprintf("Continue"), "Y")
+				}
+			} else {
+				color.Success.Println("LDAP Admin credentials successfully verified")
+				promptContinue(color.Question.Sprintf("Continue to search for DHS User Roles"), "Y")
+				return true
+			}
+		default:
+			color.Error.Printf("Bind method %s not recognized.", bindmethod)
+			retval = false
+			//os.Exit(0)
+		}
+		color.Success.Println("LDAP Default user credentials successfully verified")
+
 	}
+	promptContinue(color.Question.Sprintf("Continue to search for DHS User Roles"), "Y")
+	return retval
 }
 
 func checkRolesDN(conn *ldap.Conn, configJson ConfigJson, basedn string, memberofattribute string, ldapattribute string) {
@@ -282,7 +343,7 @@ func checkRolesDN(conn *ldap.Conn, configJson ConfigJson, basedn string, membero
 
 	banner("Searching LDAP for DHS Roles")
 
-	for _,role := range configJson.Roles {
+	for _, role := range configJson.Roles {
 		r := roleStruct{}
 		r.rolename = role[0]
 		for i := 1; i < len(role); i++ {
@@ -306,15 +367,16 @@ func checkRolesDN(conn *ldap.Conn, configJson ConfigJson, basedn string, membero
 			}
 
 			table := tablewriter.NewWriter(os.Stdout)
-			table.SetHeader([]string{"Userid", "User DN"})
-			table.SetColMinWidth(1,80)
+			table.SetHeader([]string{"Userid", "User DN", "Evaluation Duration"})
+			table.SetColMinWidth(1, 80)
 			table.SetAutoWrapText(false)
 			table.SetColumnColor(tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlueColor},
-				tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor})
+				tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor})
 
 			var accounts [][]string
 			color.Warn.Println("")
 			color.Warn.Println("")
+			start := time.Now()
 			logmsg.Println("The following users have", role[0], "Role access with External DN", role[i])
 			for _, entry := range result.Entries {
 				accounts = append(accounts, []string{entry.GetAttributeValue(ldapattribute), entry.DN})
@@ -322,18 +384,21 @@ func checkRolesDN(conn *ldap.Conn, configJson ConfigJson, basedn string, membero
 					//log.Println(spew.Sdump(entry.GetAttributeValues(viper.GetString("member-attribute"))))
 					//TODO Nested check, if no nested search requested just print the group DN details.
 					// entry.PrettyPrint(3)
-					checkNestedGroupRoles(conn, entry.GetAttributeValues(viper.GetString("member-attribute")), viper.GetString("member-attribute"),ldapattribute,table)
+					checkNestedGroupRoles(conn, entry.GetAttributeValues(viper.GetString("member-attribute")), viper.GetString("member-attribute"), ldapattribute, table)
 				} else {
-					table.Append([]string{entry.GetAttributeValue(ldapattribute),entry.DN})
+					duration := time.Since(start)
+					//logmsg.Println("Evaluation time:",duration)
+					table.Append([]string{entry.GetAttributeValue(ldapattribute), entry.DN, duration.String()})
 					//entry.PrettyPrint(3)
 				}
 			}
 
 			table.Render()
+
 		}
 		roles = append(roles, r)
 	}
-	promptContinue(color.Question.Sprintf("Continue to generate Role mapping commands"),"Y")
+	promptContinue(color.Question.Sprintf("Continue to generate Role mapping commands"), "Y")
 }
 
 func checkNestedGroupRoles(conn *ldap.Conn, values []string, getString string, ldapattribute string, table *tablewriter.Table) {
@@ -359,18 +424,17 @@ func checkNestedGroupRoles(conn *ldap.Conn, values []string, getString string, l
 				//log.Println(spew.Sdump(entry.GetAttributeValues(viper.GetString("member-attribute"))))
 				checkNestedGroupRoles(conn, entry.GetAttributeValues(viper.GetString("member-attribute")), viper.GetString("member-attribute"), ldapattribute, table)
 			} else {
-				table.Append([]string{entry.GetAttributeValue(ldapattribute),entry.DN})
+				table.Append([]string{entry.GetAttributeValue(ldapattribute), entry.DN})
 				//entry.PrettyPrint(3)
 			}
 		}
 	}
 }
 
-
 func checkAdminDN(conn *ldap.Conn, admindn string, basedn string, memberofattribute string, ldapattribute string) [][]string {
 	//color.Success.Println("Searching LDAP for Security Admin DN accounts..")
 	banner("Searching LDAP for Security Admin DN accounts")
-	filter := fmt.Sprintf("(%s=%s)",memberofattribute,admindn)
+	filter := fmt.Sprintf("(%s=%s)", memberofattribute, admindn)
 	result, err := conn.Search(ldap.NewSearchRequest(
 		basedn,
 		ldap.ScopeWholeSubtree,
@@ -379,7 +443,7 @@ func checkAdminDN(conn *ldap.Conn, admindn string, basedn string, memberofattrib
 		0,
 		false,
 		filter,
-		[]string{ldapattribute,},
+		[]string{ldapattribute},
 		nil,
 	))
 
@@ -390,18 +454,17 @@ func checkAdminDN(conn *ldap.Conn, admindn string, basedn string, memberofattrib
 
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"Userid", "User DN"})
-	table.SetColMinWidth(1,80)
+	table.SetColMinWidth(1, 80)
 	table.SetAutoWrapText(false)
 	table.SetColumnColor(tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlueColor},
 		tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor})
-
 
 	var adminaccounts [][]string
 	logmsg.Println("The following users have Security Admin Role access.")
 	color.Warn.Println("")
 	for _, entry := range result.Entries {
-		adminaccounts = append(adminaccounts,[]string{entry.GetAttributeValue(ldapattribute),entry.DN})
-		table.Append([]string{entry.GetAttributeValue(ldapattribute),entry.DN})
+		adminaccounts = append(adminaccounts, []string{entry.GetAttributeValue(ldapattribute), entry.DN})
+		table.Append([]string{entry.GetAttributeValue(ldapattribute), entry.DN})
 		//fmt.Printf(
 		//	"%s: %s \n",
 		//	color.FgCyan.Sprintf(entry.GetAttributeValue(ldapattribute)),
@@ -410,14 +473,14 @@ func checkAdminDN(conn *ldap.Conn, admindn string, basedn string, memberofattrib
 	}
 	table.Render()
 	color.Warn.Println("")
-	promptContinue(color.Question.Sprintf("Continue to verify Admin account access"),"Y")
+	promptContinue(color.Question.Sprintf("Continue to verify Admin account access"), "Y")
 	return adminaccounts
 }
 
 func checkLdapAdminBind(hostname string, port string, secure bool, bindmethod string, defaultuser string, password string) *ldap.Conn {
 	//color.Success.Println("Checking LDAP Default user credentials...")
 	banner("Checking LDAP Default user credentials")
-	net.JoinHostPort(hostname,port)
+	net.JoinHostPort(hostname, port)
 	var conn *ldap.Conn
 	var err error
 	var config *tls.Config
@@ -446,18 +509,18 @@ func checkLdapAdminBind(hostname string, port string, secure bool, bindmethod st
 	case "SIMPLE":
 		logmsg.Println("Performing simple bind")
 		err = conn.Bind(defaultuser, password)
-		if  err != nil {
+		if err != nil {
 			color.Error.Printf("Failed to bind with supplied credentials. %s", err)
 			os.Exit(0)
 		}
 	case "MD5":
 		logmsg.Println("Performing MD5 bind")
-		err = conn.MD5Bind(hostname,defaultuser, password)
-		if  err != nil {
+		err = conn.MD5Bind(hostname, defaultuser, password)
+		if err != nil {
 			if secure {
 				color.Error.Printf("MD5 Bind failed this could be because LDAPS is configured. %s", err)
 				color.Error.Printf("If you continue and the your credentials are incorrect further tests could fail.")
-				promptContinue(color.Question.Sprintf("Continue"),"Y")
+				promptContinue(color.Question.Sprintf("Continue"), "Y")
 			}
 		}
 	default:
@@ -465,11 +528,11 @@ func checkLdapAdminBind(hostname string, port string, secure bool, bindmethod st
 		os.Exit(0)
 	}
 	color.Success.Println("LDAP Default user credentials successfully verified")
-	promptContinue(color.Question.Sprintf("Continue to verify Admin accounts exist"),"Y")
+	promptContinue(color.Question.Sprintf("Continue to verify Admin accounts exist"), "Y")
 	return conn
 }
 
-func checkConnectivity(a string, h string, port string) (int, int)  {
+func checkConnectivity(a string, h string, port string) (int, int) {
 	//color.Success.Println("Checking connectivity to LDAP server hosts...")
 	banner("Checking connectivity to LDAP server hosts")
 
@@ -481,21 +544,21 @@ func checkConnectivity(a string, h string, port string) (int, int)  {
 
 	failures := 0
 	successes := 0
-	timeoutDuration := time.Duration(10) * time.Millisecond
-	intervalDuration := time.Duration(1) * time.Millisecond
+	timeoutDuration := time.Duration(3000) * time.Millisecond
+	intervalDuration := time.Duration(2) * time.Millisecond
 	schema := ping.TCP.String()
 	protocol, _ := ping.NewProtocol(schema)
 	p, _ := strconv.Atoi(port)
 	for i := range hosts {
 		table := tablewriter.NewWriter(os.Stdout)
 		table.SetHeader([]string{hosts[i]})
-		table.SetColMinWidth(0,100)
+		table.SetColMinWidth(0, 100)
 		table.SetAutoWrapText(false)
 		table.SetHeaderColor(tablewriter.Colors{tablewriter.Bold, tablewriter.FgGreenColor})
 		table.SetColumnColor(tablewriter.Colors{tablewriter.Bold, tablewriter.FgBlueColor})
-		addr,err := net.LookupIP(hosts[i])
+		addr, err := net.LookupIP(hosts[i])
 		if err != nil {
-			color.Error.Println("Unable to resolve hostname: ",hosts[i])
+			color.Error.Println("Unable to resolve hostname: ", hosts[i])
 			failures += 1
 			break
 		}
@@ -504,7 +567,7 @@ func checkConnectivity(a string, h string, port string) (int, int)  {
 			Interval: intervalDuration,
 			Host:     addr[0].String(),
 			Port:     p,
-			Counter:  3,
+			Counter:  5,
 			Protocol: protocol,
 		}
 		pinger := ping.NewTCPing()
@@ -517,14 +580,14 @@ func checkConnectivity(a string, h string, port string) (int, int)  {
 			break
 		}
 		var result []string
-		result = append(result, fmt.Sprintf("%s",pinger.Result() ))
+		result = append(result, fmt.Sprintf("%s", pinger.Result()))
 		table.Append(result)
 		table.Render()
 
 		failures += pinger.Result().Failed()
 		successes += pinger.Result().SuccessCounter
 	}
-	return successes,failures
+	return successes, failures
 
 }
 
@@ -534,13 +597,13 @@ func validateField(key string, masked bool) string {
 	} else {
 		field, err := getFieldValue(key, masked)
 		if err != nil {
-			panic (err)
+			panic(err)
 		}
 		return field
 	}
 }
 
-func getFieldValue(key string, masked bool) (string, error){
+func getFieldValue(key string, masked bool) (string, error) {
 	if masked {
 		prompt := promptui.Prompt{
 			Label: key,
@@ -560,7 +623,7 @@ func promptContinue(pstring string, def string) {
 	prompt := promptui.Prompt{
 		Label:     pstring,
 		IsConfirm: true,
-		Default: cont,
+		Default:   cont,
 	}
 	result, _ := prompt.Run()
 	if result == "" {
@@ -583,10 +646,8 @@ func contains(arr []string, str string) bool {
 func banner(s string) {
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{s})
-	table.SetColMinWidth(0,100)
+	table.SetColMinWidth(0, 100)
 	table.SetAutoWrapText(false)
 	table.SetHeaderColor(tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiWhiteColor, tablewriter.BgBlueColor})
 	table.Render()
 }
-
-
